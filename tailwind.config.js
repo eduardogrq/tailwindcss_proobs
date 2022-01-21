0a180cf9a9762422939fe48d1094af7b96d2005f
Eduardo Quiñonez <40242071+eduardogrq@users.noreply.github.com>
2022-01-21 14:17:39 -0600

    Styles applied to our layout

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // We have to specify here where is gonna be compiled our styles
+  content: [
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
